064798cecd5afaa5fc1f5d2c458916f893e86583
Dawid Kowal <dawidkowal0@gmail.com>
2023-12-03 15:40:45 +0100

    fixed php composer issues

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,9 @@
-<?php ?>
-Hello
+<?php
+require_once '/var/www/html/composer/vendor/autoload.php';
+
+$loader = new \Twig\Loader\ArrayLoader([
+    'index' => 'Hello {{ name }}!',
+]);
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index', ['name' => 'Fabien']);
