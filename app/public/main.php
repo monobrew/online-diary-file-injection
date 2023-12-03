3195b78bb91db33253201f15bd5ab3efb5aa6aea
Dawid Kowal <dawidkowal0@gmail.com>
2023-12-04 00:15:28 +0100

    added diary entry insertion

--- app/public/main.php
+++ app/public/main.php
@@ -1,16 +1,31 @@
 <?php
 if (!defined('IN_INDEX')) { exit("This file can only be included in index.php."); }
 
-if (isset($_POST['title'])) {
+$user_id = 1;
 
+if (isset($_POST['title']) && isset($_POST['text']) ) {
+    
+    $title = $_POST['title'];
+    $text = $_POST['text'];
+    $date = date("Y-m-d");
+
+    $stmt = DB::getInstance()->prepare("INSERT INTO notes (user_id, title, text, date) VALUES (:user_id, :title, :text, :date)");
+    $stmt->execute([
+        ':user_id' => $user_id,
+        ':title' => $title,
+        ':text' => $text,
+        ':date' => $date
+    ]);
+
+    unset($_POST['title']);
+    unset($_POST['text']);
 }
 
 $twig_loader = new \Twig\Loader\FilesystemLoader('./templates');
 $twig = new \Twig\Environment($twig_loader);
 
-$user_id = 1;
 
-$stmt = DB::getInstance()->prepare("SELECT title, text, date FROM notes WHERE user_id=:user_id");
+$stmt = DB::getInstance()->prepare("SELECT title, text, date FROM notes WHERE user_id=:user_id ORDER BY date, id DESC");
 $stmt->execute([
     ':user_id' => $user_id
 ]);
