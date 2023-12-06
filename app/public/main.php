2d50d5c961f1d2e85a0befd0df869722f3ac1971
Dawid Kowal <dawidkowal0@gmail.com>
2023-12-06 14:40:19 +0100

    working image upload

--- app/public/main.php
+++ app/public/main.php
@@ -3,8 +3,16 @@ if (!defined('IN_INDEX')) { exit("This file can only be included in index.php.")
 
 $user_id = 1;
 
+$stmt = DB::getInstance()->prepare("SELECT filename FROM notes, media, users WHERE notes.id=media.note_id AND notes.user_id=:user_id");
+$stmt->execute([
+    ':user_id' => $user_id
+]);
 
-$stmt = DB::getInstance()->prepare("SELECT notes.id, notes.title, notes.text, media.filename date FROM notes LEFT JOIN media ON notes.id=media.note_id WHERE user_id=:user_id ORDER BY date DESC, id DESC");
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    copy(STORAGE_PATH . '/' . $row["filename"], __DIR__ . '/img/' . $row["filename"]); 
+}
+
+$stmt = DB::getInstance()->prepare("SELECT title, text, filename, date FROM notes LEFT JOIN media ON notes.id=media.note_id WHERE user_id=:user_id ORDER BY date DESC, notes.id DESC");
 $stmt->execute([
     ':user_id' => $user_id
 ]);
@@ -13,14 +21,12 @@ $notes = array();
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     if (isset($row["filename"])) {
-        array_push($notes, array("id" => $row["id"],
-                                 "text" => $row["text"],
+        array_push($notes, array("text" => $row["text"],
                                  "title" => $row["title"],
                                  "date" => $row["date"],
                                  "filename" => $row["filename"]));
     } else {
-        array_push($notes, array("id" => $row["id"],
-                                 "text" => $row["text"],
+        array_push($notes, array("text" => $row["text"],
                                  "title" => $row["title"],
                                  "date" => $row["date"],
                                  "filename" => ""));
